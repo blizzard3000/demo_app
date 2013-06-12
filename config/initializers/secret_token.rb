3336627a135bbf0685c78a6ba7d9b516b3298cb6
blizzard3000 <blizzard3000@me.com>
2013-06-12 15:35:47 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'eddee185d7dbbec6583b10097891d4912be06173871406aaa29a7cadd9a4324dda56f1fc3a8989427872109a7f3ac7f1479be316d3ca23d1777bf77dcfea1b7a'
